eb165b52828068ecf3b17a72fdece363861a3627
Tazmin-Far <51744990+Tazmin-Far@users.noreply.github.com>
2019-06-12 16:16:51 +0530

    First

---
+++ Atom/Atom/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Atom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Pb = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Atuo = new System.Windows.Forms.CheckBox();
+            this.ENX = new System.Windows.Forms.TextBox();
+            this.MPX = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.DiffXY = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AccXY = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Pb
+            // 
+            this.Pb.BackColor = System.Drawing.Color.White;
+            this.Pb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Pb.Location = new System.Drawing.Point(0, 0);
+            this.Pb.Name = "Pb";
+            this.Pb.Size = new System.Drawing.Size(737, 418);
+            this.Pb.TabIndex = 1;
+            this.Pb.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Atuo);
+            this.panel1.Controls.Add(this.ENX);
+            this.panel1.Controls.Add(this.MPX);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.DiffXY);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.AccXY);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 369);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(737, 49);
+            this.panel1.TabIndex = 7;
+            // 
+            // Atuo
+            // 
+            this.Atuo.AutoSize = true;
+            this.Atuo.Location = new System.Drawing.Point(303, 29);
+            this.Atuo.Name = "Atuo";
+            this.Atuo.Size = new System.Drawing.Size(48, 17);
+            this.Atuo.TabIndex = 17;
+            this.Atuo.Text = "Auto";
+            this.Atuo.UseVisualStyleBackColor = true;
+            // 
+            // ENX
+            // 
+            this.ENX.Location = new System.Drawing.Point(197, 26);
+            this.ENX.Name = "ENX";
+            this.ENX.Size = new System.Drawing.Size(100, 20);
+            this.ENX.TabIndex = 16;
+            this.ENX.Text = "1";
+            // 
+            // MPX
+            // 
+            this.MPX.Location = new System.Drawing.Point(197, 3);
+            this.MPX.Name = "MPX";
+            this.MPX.Size = new System.Drawing.Size(100, 20);
+            this.MPX.TabIndex = 15;
+            this.MPX.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(165, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "EN:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(165, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "MP:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(569, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // DiffXY
+            // 
+            this.DiffXY.AutoSize = true;
+            this.DiffXY.Location = new System.Drawing.Point(85, 22);
+            this.DiffXY.Name = "DiffXY";
+            this.DiffXY.Size = new System.Drawing.Size(35, 13);
+            this.DiffXY.TabIndex = 11;
+            this.DiffXY.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "X and Y Diff";
+            // 
+            // AccXY
+            // 
+            this.AccXY.AutoSize = true;
+            this.AccXY.Location = new System.Drawing.Point(85, 6);
+            this.AccXY.Name = "AccXY";
+            this.AccXY.Size = new System.Drawing.Size(35, 13);
+            this.AccXY.TabIndex = 9;
+            this.AccXY.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Acc X and Y";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(650, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 418);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Pb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Pb)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Pb;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label DiffXY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label AccXY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ENX;
+        private System.Windows.Forms.TextBox MPX;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox Atuo;
+    }
+}
+
---
+++ Atom/Atom/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atom
+{
+    public partial class Form1 : Form
+    {
+        int[] pmxi = new int[400];
+        int[] pmyi = new int[400];
+ 
+        public struct Charge
+        {
+            public double x;
+            public double y;
+            public double z;
+
+            public double AccX;
+            public double AccY;
+            public double AccZ;
+
+            public double strength;
+        }
+
+        public struct MP
+        {
+            public double x;
+            public double y;
+            public double z;
+
+            public double AccX;
+            public double AccY;
+            public double AccZ;
+
+            public double strength;
+        }
+
+        public Charge[] c = new Charge[400];
+        public MP[] m = new MP[400];
+
+        public int[] opxi = new int[400];
+        public int[] opyi = new int[400];
+
+        //Number of electrons to calcute
+        private int cc = 1;
+        private int mm = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            if (timer1.Enabled)
+                button1.Text = "Stop";
+            else
+                button1.Text = "Start";
+        }
+
+        private void Plot()
+        {
+            Pen ClearPen = new Pen(Color.White);
+            Pen myPen = new Pen(Color.Blue, 1);
+            Graphics g = Pb.CreateGraphics();
+
+            for (int x = 0; x < cc; x++)
+            {
+                double px = c[x].x + Pb.Width / 2;
+                double py = c[x].y + Pb.Height / 2;
+                int pxi = Convert.ToInt32(px + 0.5);
+                int pyi = Convert.ToInt32(py + 0.5);
+                g.DrawEllipse(ClearPen, opxi[x], opyi[x], Convert.ToInt16(c[x].strength)*3, Convert.ToInt16(c[x].strength)*3);
+                g.DrawEllipse(myPen, pxi, pyi, Convert.ToInt16(c[x].strength)*3, Convert.ToInt16(c[x].strength)*3);
+                opxi[x] = pxi;
+                opyi[x] = pyi;
+            }
+            g.Dispose();
+            myPen.Dispose();
+            ClearPen.Dispose();
+        }
+
+        private void PlotMP()
+        {
+            for (int y = 0; y < mm; y++)
+            {
+                double mx = m[y].x + Pb.Width / 2;
+                double my = m[y].y + Pb.Height / 2;
+                int mxi = Convert.ToInt32(mx + 0.5);
+                int myi = Convert.ToInt32(my + 0.5);
+
+
+                SolidBrush myBrush = new SolidBrush(Color.Red);
+                SolidBrush CBrush = new SolidBrush(Color.White);
+
+                Graphics g = Pb.CreateGraphics();
+
+                Rectangle rect = new Rectangle(pmxi[y]-Convert.ToInt16(m[y].strength)/2, pmyi[y]-Convert.ToInt16(m[y].strength)/2, Convert.ToInt16(m[y].strength*4), Convert.ToInt16(m[y].strength*4));
+
+                //g.DrawEllipse(CPen, pmxi, pmyi, 4, 4);
+                //g.DrawEllipse(CPen, pmxi1, pmyi1, 4, 4);
+                g.FillEllipse(CBrush, rect);
+
+                rect = new Rectangle(mxi-Convert.ToInt16(m[y].strength)/2, myi-Convert.ToInt16(m[y].strength)/2, Convert.ToInt16(m[y].strength*4), Convert.ToInt16(m[y].strength*4));
+                //g.DrawEllipse(myPen, mxi, myi, 4, 4);
+                //g.DrawEllipse(myPen, mxi1, myi1, 4, 4);
+
+                g.FillEllipse(myBrush, rect);
+
+                pmxi[y] = mxi;
+                pmyi[y] = myi;
+                g.Dispose();
+                myBrush.Dispose();
+                CBrush.Dispose();
+            }
+        }
+
+        private void CalculateAttraction()
+        {
+            //Attraction toward Matter Particle
+            for (int y = 0; y < mm; y++)
+            {
+                for (int x = 0; x < cc; x++)
+                {
+                    //AccXY.Text = dist.ToString();
+                    double Dist =  Math.Sqrt(Math.Pow(m[y].x - c[x].x, 2) + Math.Pow(m[y].y - c[x].y, 2));
+                    double XDist = m[y].x - c[x].x;
+                    double YDist = m[y].y - c[x].y;
+
+                    double AccXAdd = (1 / Dist) * XDist * ((m[y].strength*1000 + c[x].strength) / Dist);
+                    double AccYAdd = (1 / Dist) * YDist * ((m[y].strength*1000 + c[x].strength) / Dist);
+
+                    if (AccXAdd > 1)
+                        AccXAdd = 1;
+                    if (AccYAdd > 1)
+                        AccYAdd = 1;
+                    if (AccXAdd < -1)
+                        AccXAdd = -1;
+                    if (AccYAdd < -1)
+                        AccYAdd = -1;
+
+                    c[x].AccX = c[x].AccX - AccXAdd;
+                    c[x].AccY = c[x].AccY - AccYAdd;
+                }
+            }
+            //AccXY.Text = c[1].AccX.ToString();
+            
+            //Replusion from Energy (particle)
+            
+            for (int x = 0; x < cc; x++)
+            {
+                for (int x1 = 0; x1 < cc; x1++)
+                {
+                    if (x1 != x)
+                    {
+                        double Dist = Math.Sqrt(Math.Pow(c[x1].x - c[x].x, 2) + Math.Pow(c[x1].y - c[x].y, 2));
+                        double XDist = c[x1].x - c[x].x;
+                        double YDist = c[x1].y - c[x].y;
+
+                        double AccXAdd = (1 / Dist) * XDist * ((c[x1].strength/5 + c[x].strength/5) / Dist);
+                        double AccYAdd = (1 / Dist) * YDist * ((c[x1].strength/5 + c[x].strength/5) / Dist);
+
+
+                        if (AccXAdd > 1)
+                            AccXAdd = 1;
+                        if (AccYAdd > 1)
+                            AccYAdd = 1;
+                        if (AccXAdd < -1)
+                            AccXAdd = 1;
+                        if (AccYAdd < -1)
+                            AccYAdd = 1;
+
+
+                        c[x].AccX = c[x].AccX + AccXAdd;
+                        c[x].AccY = c[x].AccY + AccYAdd;
+                    }
+                }
+            }
+
+            //DiffXY.Text = c[1].AccX.ToString();
+
+            for (int x = 0; x < cc; x++)
+            {
+                c[x].x = c[x].x - c[x].AccX;
+                c[x].y = c[x].y - c[x].AccY;
+            }
+
+
+
+            for (int x = 0; x < cc; x++)
+                for (int y = 0; y < mm; y++)
+                {
+                    {
+                        //AccXY.Text = dist.ToString();
+                        double Dist = Math.Sqrt(Math.Pow(m[y].x - c[x].x, 2) + Math.Pow(m[y].y - c[x].y, 2));
+                        double XDist = m[y].x - c[x].x;
+                        double YDist = m[y].y - c[x].y;
+
+                        //double AccXAdd = ((1 / Dist) * XDist * ((m[y].strength + c[x].strength) / Dist));
+                        double AccXAdd = ((1 / Dist) * XDist * (1 / Dist));
+                        double AccYAdd = ((1 / Dist) * YDist * (1 / Dist));
+
+                        if (AccXAdd > 1)
+                            AccXAdd = 1;
+                        if (AccYAdd > 1)
+                            AccYAdd = 1;
+                        if (AccXAdd < -1)
+                            AccXAdd = -1;
+                        if (AccYAdd < -1)
+                            AccYAdd = -1;
+
+
+                        m[y].AccX = m[y].AccX - AccXAdd/1000;
+                        m[y].AccY = m[y].AccY - AccYAdd/1000;
+                    }
+                }
+
+            for (int y = 0; y < mm; y++)
+            {
+                m[y].x = m[y].x + m[y].AccX;
+                m[y].y = m[y].y + m[y].AccY;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            CalculateAttraction();
+            Plot();
+            PlotMP();
+            timer1.Enabled = true;
+        }
+
+        private void LoadData()
+        {
+            /*c[0].x = 90;
+            c[0].y = 54;
+            c[0].z = 0;
+            c[0].AccX = 0.5;
+            c[0].AccY = 0;
+            c[0].AccZ = 0;
+            c[0].strength = 0.01;*/
+
+            Random r = new Random();
+
+            for (int i = 0; i <= 390; i++)
+            {
+                c[i].x = r.Next(1000)-500;
+                c[i].y = r.Next(500)-250;
+                c[i].z = 0;
+                c[i].AccX = 0;
+                c[i].AccY = 0;
+                c[i].AccZ = 0;
+                c[i].strength = r.Next(10, 5000) / 1000;
+            }
+
+
+
+
+
+
+
+
+
+            /*m[0].x = 100;
+            m[0].y = 0;
+            m[0].AccX = 0;
+            m[0].AccY = 0;
+            m[0].AccZ = 0;
+            m[0].strength = 2;
+
+            m[1].x = -300;
+            m[1].y = 0;
+            m[1].AccX = 0;
+            m[1].AccY = 0;
+            m[1].AccZ = 0;
+            m[1].strength = 5;
+            */
+
+            int CXN = 0;
+
+            for (int x = 0; x <= 399; x++)
+            {
+                m[x].x = r.Next(-500,500);
+                m[x].y = r.Next(-500, 500);
+                m[x].AccX = 0;
+                m[x].AccY = 0;
+                m[x].AccZ = 0;
+                m[x].strength = r.Next(10, 120) / 10;
+                if (Atuo.Checked)
+                {
+
+                    c[CXN].x = m[x].x - 5;
+                    c[CXN].y = m[x].y - 5;
+                    c[CXN].z = 0;
+                    c[CXN].AccX = 0;
+                    c[CXN].AccY = 0;
+                    c[CXN].AccZ = 0;
+                    c[CXN].strength = m[x].strength/5;
+                    CXN++;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Pb.Refresh();
+            cc = Convert.ToInt16(ENX.Text);
+            mm = Convert.ToInt16(MPX.Text);
+            LoadData();
+        }
+    }
+}
